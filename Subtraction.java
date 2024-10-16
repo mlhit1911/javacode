415101040403522d10be55f2353ebbcd9039009b
mlhit1911 <m.lakshumaiah@klh.edu.in>
2024-10-16 11:59:56 +0530

    jfiles

---
+++ Subtraction.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class Subtraction
+{
+public static void main(String args[])
+{
+int a=10,b=20,c;
+c=a-b;
+System.out.println(c);
+/* asjahjdka
+  askjh
+  sakjd
+  AJSD
+  aksjd
+  */
+
+  
+}
+}
